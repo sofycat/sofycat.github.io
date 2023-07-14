d65b4c6ab1ad19cd64849756c219a49f04c211af
Sofy-dev <youraveragesophie@gmail.com>
2023-07-14 22:25:42 +1000

    site wip :3

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('div').addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+})
+document.querySelector('img').addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+})
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+})
